8e34ecc8f5786c43c1608f66371c8f256c564d3d
Farsane <soufien.farsane-etu@etu.univh2c.ma>
2024-02-19 11:53:52 +0100

    2

--- IOC-ActivitePratique/src/main/java/Presentation/Pres2.java
+++ IOC-ActivitePratique/src/main/java/Presentation/Pres2.java
@@ -1,13 +1,28 @@
 package Presentation;
 
 import Dao.DaoImplV2;
+import Dao.IDao;
+import Metier.IMetier;
 import Metier.MetierImpl;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
+
 public class Pres2 {
-    public static void main(String[] args) {
-        DaoImplV2 dao = new DaoImplV2();
-        MetierImpl metier = new MetierImpl();
-        metier.setDao(dao);
-        System.out.println("RES = "+ metier.calcul());
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        Scanner scanner=new Scanner(new File("config.txt"));
+        String daoClassName=scanner.nextLine();
+        Class cDao=Class.forName(daoClassName);
+        IDao dao=(IDao) cDao.getConstructor().newInstance();
+        System.out.println(dao.getData());
+        String metierClassName=scanner.nextLine();
+        Class cMetier=Class.forName(metierClassName);
+
+        IMetier metier=(IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+        System.out.println(metier.calcul());
     }
 }
